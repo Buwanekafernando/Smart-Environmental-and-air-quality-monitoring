ba8db8b9da0dbe7abf5966ac452f275945a99c20
Buwanekafernando <fernandodeshan0@gmail.com>
2026-04-15 17:09:05 +0530

    update the app.py for adding api get and data take from serial_reader

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,24 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+from db import collection
+import threading
+from serial_reader import generate_data
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/data", methods=["GET"])
+# Start background data generation
+threading.Thread(target=generate_data, daemon=True).start()
+
+# API to get latest data
+@app.route("/api/data", methods=["GET"])
 def get_data():
-    data = list(collection.find({}, {"_id": 0}))
+    data = list(collection.find().sort("timestamp", -1).limit(20))
+
+    for d in data:
+        d["_id"] = str(d["_id"])  # convert ObjectId
+
     return jsonify(data)
 
-app.run(port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
