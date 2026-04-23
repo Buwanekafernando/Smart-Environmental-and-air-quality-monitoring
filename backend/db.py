937b758736d6835e111424952e91fccc6cfbf92d
Buwanekafernando <fernandodeshan0@gmail.com>
2026-04-23 12:56:53 +0530

    change the timestamps

--- backend/db.py
+++ backend/db.py
@@ -2,12 +2,12 @@ from pymongo import MongoClient
 from datetime import datetime
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client["iot_database"]
+db = client["iot_db"]
 collection = db["sensor_data"]
 
 def insert_sensor_data(data):
-    data["created_at"] = datetime.utcnow()
+    data["timestamp"] = datetime.utcnow()
     collection.insert_one(data)
 
 def get_latest_data(limit=20):
-    return list(collection.find().sort("created_at", -1).limit(limit))
+    return list(collection.find().sort("timestamp", -1).limit(limit))
